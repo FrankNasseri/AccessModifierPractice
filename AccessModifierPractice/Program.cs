70d1db1e337147ebd4d85ab75862722d7a54cd21
Frank Nasseri <franknasseri57@gmail.com>
2017-07-11 15:16:06 -0400

    added notes to access modifier

--- AccessModifierPractice/Program.cs
+++ AccessModifierPractice/Program.cs
@@ -10,6 +10,13 @@ namespace AccessModifierPractice
     {
         static void Main(string[] args)
         {
+            //work in pairs
+            //on paper or in a word processor
+            //create a call cell phone determine 
+            //3 fields 
+            //2 properties
+            //4 methods
+            //for each of the numbers you created, determine whether it should be public or private
         }
     }
 }
